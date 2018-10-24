c25930a01b326a79906325b5a530291f0b306c7f
Michael Cook <michael.cook@techdept.co.uk>
2018-10-24 16:25:58 +0100

    Stuff is happening. No content object? though?

---
+++ src/templates/post.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Layout from "../components/Layout";
+
+export default () => (
+  <Layout>
+    <h1>Post</h1>
+    <p>This will probably look like blog-post.js</p>
+  </Layout>
+);
